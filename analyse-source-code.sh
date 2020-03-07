e95edc6425b344c49bd52bd3275c10b565b30281
Volodymyr Yahello <vyahello@gmail.com>
2020-03-07 20:45:29 +0200

    Introduce travis CI

---
+++ analyse-source-code.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+PACKAGE="billtracker"
+
+
+--entry-point-box() {
+:<<DOC
+    Provides pretty-printer check box
+DOC
+    echo "Start ${1} analysis ..."
+}
+
+
+remove-pycache() {
+:<<DOC
+    Removes python cache directories
+DOC
+    ( find . -d -name __pycache__ | xargs rm -r )
+}
+
+
+check-black() {
+:<<DOC
+    Runs "black" code analyser
+DOC
+    --entry-point-box "black" && ( black --check ${PACKAGE} )
+}
+
+
+check-flake8() {
+:<<DOC
+    Runs "flake8" code analysers
+DOC
+    --entry-point-box "flake" && ( flake8 ${PACKAGE} )
+}
+
+
+check-flakehell() {
+:<<DOC
+    Runs "flakehell" code analysers
+DOC
+    --entry-point-box "flakehell" && ( flakehell lint )
+}
+
+
+check-flake() {
+:<<DOC
+    Runs "flake8" code analysers
+DOC
+    check-flake8 && check-flakehell
+}
+
+
+check-pylint() {
+:<<DOC
+    Runs "pylint" code analyser
+DOC
+    --entry-point-box "pylint" && ( pylint $(find ./ -iname *.py) )
+}
+
+
+check-mypy() {
+:<<DOC
+    Runs "mypy" code analyser
+DOC
+    --check-box "mypy" && ( mypy --package "${PACKAGE}" )
+}
+
+
+check-unittests() {
+:<<DOC
+    Runs unittests using "pytest" framework
+DOC
+    --entry-point-box "unitests" && pytest
+}
+
+
+main() {
+:<<DOC
+    Runs "main" code analyser
+DOC
+    remove-pycache
+    check-black && check-flake && check-mypy && check-pylint && check-unittests
+}
+
+
+main
