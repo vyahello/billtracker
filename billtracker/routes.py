809c4a99974a735ca9f0c16b35ccc1c505646d25
Volodymyr Yahello <vyahello@gmail.com>
2020-03-08 12:01:04 +0200

    Include mock storage data structure

--- billtracker/routes.py
+++
@@ -1,8 +0,0 @@
-"""Module contains a list of routes."""
-from pyramid.config import ViewsConfiguratorMixin
-
-
-def includeme(config: ViewsConfiguratorMixin) -> None:
-    """Returns content of includeme route."""
-    config.add_static_view("static", "static", cache_max_age=3600)
-    config.add_route("home", "/")
--- billtracker/views/default.py
+++
@@ -1,9 +0,0 @@
-"""Module to contain not found views."""
-from typing import Dict
-from pyramid.view import view_config
-
-
-@view_config(route_name="home", renderer="../templates/home/default.pt")
-def home(_) -> Dict[str, str]:
-    """Returns home page for `Bills Tracker` tool."""
-    return {"project": "Bills Tracker"}
