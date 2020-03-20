9d1906bd6a9f08a4b67cce0050ef158f6385c7cb
Volodymyr Yahello <vyahello@gmail.com>
2020-03-20 19:58:08 +0200

    Implement get details route

--- billtracker/routes.py
+++ billtracker/routes.py
@@ -6,3 +6,4 @@ def includeme(config: ViewsConfiguratorMixin) -> None:
     """Returns content of includeme route."""
     config.add_static_view("static", "static", cache_max_age=3600)
     config.add_route("home", "/")
+    config.add_route("details", "/bill/{bill_id}")
--- billtracker/views/default.py
+++ billtracker/views/default.py
@@ -1,14 +1,23 @@
 """Module to contain not found views."""
-from typing import Dict, Optional
+from typing import Dict, Optional, Union, Any
 from pyramid.request import Request
+from pyramid.response import Response
 from pyramid.view import view_config
 from billtracker.data.repository import user_by_id
 from billtracker.data.models.users import User
+from data import repository
 
 
 @view_config(route_name="home", renderer="../templates/home/default.pt")
 def home(_: Request) -> Dict[str, Optional[User]]:
     """Returns index page content."""
     return {
-        "user": user_by_id(1),
+        "user": user_by_id(user_id=1),
     }
+
+
+@view_config(route_name="details", renderer="../templates/home/details.pt", request_method="GET")
+def get_details(request: Request) -> Union[Response, Dict[str, Any]]:
+    """Returns bill details."""
+    bill = repository.bill_by_id(bill_id=int(request.matchdict.get("bill_id")))
+    return Response(status=404) if not bill else {"user": user_by_id(user_id=1), "bill": bill, "error": None}
